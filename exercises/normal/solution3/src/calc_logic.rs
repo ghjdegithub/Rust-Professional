a54cdee9746cd25853cc6b6a7120da7b61865670
gonghuijun <303690073@qq.com>
2025-02-11 15:16:44 +0800

    23

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,14 @@
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    if n > 365 {
+        return 1.0;
+    }
+
+    // 计算没有任何两个人生日相同的概率
+    let mut prob = 1.0;
+    for i in 0..n {
+        prob *= (365 - i) as f64 / 365f64;
+    }
+
+    // 计算至少有两个人生日相同的概率
+    1.0 - prob
 }
