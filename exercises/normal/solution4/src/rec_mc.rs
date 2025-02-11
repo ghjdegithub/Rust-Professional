bc62e49d5aebc5238afc286784231939913b5523
gonghuijun <303690073@qq.com>
2025-02-11 15:33:46 +0800

    24

--- exercises/normal/solution4/src/rec_mc.rs
+++ exercises/normal/solution4/src/rec_mc.rs
@@ -1,4 +1,16 @@
+use std::cmp::min;
+
 pub fn dp_rec_mc(amount: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    let cashes = [1, 2, 5, 10, 20, 30, 50, 100];
+    let mut rec = vec![0; (amount + 1) as usize];
+    for i in 1..=amount {
+        let mut min_cashes_num = i;
+        for c in cashes.iter().filter(|c| *c <= &i) {
+            let curr = rec[(i - c) as usize] + 1;
+            min_cashes_num = min(min_cashes_num, curr);
+        }
+        rec[i as usize] = min_cashes_num;
+    }
+
+    rec[amount as usize] as u32
 }
