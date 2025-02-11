6bc0f7d95ac348f494c31068c44d87295af800b2
gonghuijun <303690073@qq.com>
2025-02-11 15:38:57 +0800

    25

--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,4 +1,17 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    let mut fib1 = 0;
+    let mut fib2 = 1;
+    let mut sum = 0;
+
+    while fib2 <= threshold {
+        if fib2 % 2 != 0 {
+            sum += fib2;
+        }
+
+        let next_fib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next_fib;
+    }
+
+    sum
 }
